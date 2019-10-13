b9907f645298d757b0c97b1f4391eb8ec3998b22
kthatoto <kthatoto@gmail.com>
2019-10-14 01:21:05 +0900

    Add routines column component

---
+++ src/components/RoutinesColumn.vue
@@ -0,0 +1,67 @@
+<template lang="pug">
+.routine(@click="showingMenu = false")
+  .routine__header
+    div {{ dateLabel }}
+    h2
+      span {{ title }}
+      span(@click.stop)
+        icon.icon(name="ellipsis-v" @click.native="showingMenu = !showingMenu")
+    .routine__headerMenu(v-show="showingMenu")
+      .routine__headerMenuItem Create
+      .routine__headerMenuItem Edit
+      .routine__headerMenuItem Delete
+</template>
+
+<script>
+export default {
+  props: {
+    type: { type: String, required: true }, // 'daily' | 'weekly' | 'monthly'
+    dateLabel: { type: String, required: true }
+  },
+  data () {
+    return {
+      showingMenu: false
+    }
+  },
+  computed: {
+    title () {
+      return this.type.charAt(0).toUpperCase() + this.type.slice(1) + ' Routines'
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.routine
+  background-color: lightgray
+  min-width: 300px
+  width: 300px
+  height: 500px
+  margin-right: 15px
+  padding: 10px
+  border-radius: 5px
+  &__header
+    position: relative
+    padding-bottom: 5px
+    border-bottom: 1px solid gray
+    h2
+      display: flex
+      justify-content: space-between
+      align-items: center
+      .icon
+        width: 20px
+        height: 20px
+        cursor: pointer
+    &Menu
+      position: absolute
+      top: 100%
+      right: -10px
+      background-color: gray
+      &Item
+        width: 100px
+        color: white
+        margin: 0 5px
+        padding: 5px 2px
+        &:not(:last-child)
+          border-bottom: 1px solid white
+</style>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,53 @@
 <template lang="pug">
 .index
   .index__datepicker
-    icon.icon(name="chevron-left")
-    .index__date {{ displayDate }}
-    icon.icon(name="chevron-right")
+    icon.icon(name="chevron-left" @click.native="prevDate")
+    .index__date {{ displayHeaderDate }}
+    icon.icon(name="chevron-right" @click.native="nextDate")
+  .index__routines
+    routines-column(type="daily" :dateLabel="displayDate")
+    routines-column(type="weekly" :dateLabel="displayWeek")
+    routines-column(type="monthly" :dateLabel="displayMonth")
 </template>
 
 <script>
+import RoutinesColumn from '~/components/RoutinesColumn'
 export default {
+  components: { RoutinesColumn },
   data () {
     return {
-      date: new Date()
+      date: new Date(),
+      dailyRoutines: [],
+      weeklyRoutines: [],
+      monthlyRoutines: []
     }
   },
   computed: {
-    displayDate () {
+    displayHeaderDate () {
       return `${this.date.getFullYear()}/${this.date.getMonth() + 1}/${this.date.getDate()}`
+    },
+    beginingOfWeekDate () {
+      return new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - this.date.getDay())
+    },
+    endOfWeekDate () {
+      return new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + (7 - this.date.getDay()))
+    },
+    displayDate () {
+      return `${this.date.getMonth() + 1}/${this.date.getDate()}`
+    },
+    displayWeek () {
+      return `${this.beginingOfWeekDate.getMonth() + 1}/${this.beginingOfWeekDate.getDate()}~${this.endOfWeekDate.getMonth() + 1}/${this.endOfWeekDate.getDate()}`
+    },
+    displayMonth () {
+      return `${this.date.getFullYear()}/${this.date.getMonth() + 1}`
+    }
+  },
+  methods: {
+    prevDate () {
+      this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() - 1)
+    },
+    nextDate () {
+      this.date = new Date(this.date.getFullYear(), this.date.getMonth(), this.date.getDate() + 1)
     }
   }
 }
@@ -23,6 +55,24 @@ export default {
 
 <style lang="stylus" scoped>
 .index
+  padding: 50px
   &__datepicker
+    display: flex
+    justify-content: space-between
+    background-color: lightgray
+    border-radius: 8px
+    width: 250px
+    vertical-align: middle
+    margin-bottom: 20px
+    .icon
+      padding: 3px 0
+      width: 30px
+      height: 30px
+      cursor: pointer
+  &__date
+    height: 30px
+    line-height: 30px
+    text-align: center
+  &__routines
     display: flex
 </style>
