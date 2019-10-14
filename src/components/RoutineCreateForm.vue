7fde884a61b8f79e8281c00751425d29afd4d6ce
kthatoto <kthatoto@gmail.com>
2019-10-14 13:58:28 +0900

    Create form

---
+++ src/components/RoutineCreateForm.vue
@@ -0,0 +1,144 @@
+<template lang="pug">
+.create
+  .create__checkbox
+  .create__form
+    .create__formTop
+      .create__count
+        .create__countLabel count
+        .create__countButton.-hover(:class="{'-active': countActive}"
+          @click="countActive = !countActive")
+            .create__countButtonCircle(:class="{'-active': countActive}")
+        input.create__countInput(v-model="count" :disabled="!countActive")
+      .create__title
+        .create__titleLabel title
+        input.create__titleInput(v-model="title")
+    .create__formBottom
+      .create__description
+        .create__descriptionLabel description
+        textarea.create__descriptionInput(v-model="description")
+    .create__formButtons
+      .create__formButtonsItem.-hover(@click="cancel")
+        icon.icon(name="ban")
+        span cancel
+      .create__formButtonsItem.-hover.-create(@click="submit")
+        icon.icon(name="plus")
+        span create
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      countActive: false,
+      count: 1,
+      title: '',
+      description: ''
+    }
+  },
+  methods: {
+    cancel () {
+      this.$emit('cancel')
+    },
+    submit () {
+    }
+  },
+  watch: {
+    countActive () {
+      if (!this.countActive) {
+        this.count = 1
+      }
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+.create
+  display: flex
+  padding: 5px 0
+  &__checkbox
+    border: 2px solid black
+    border-radius: 5px
+    width: 30px
+    height: 30px
+    background-color: white
+    cursor: pointer
+    margin-top: 15px
+  &__form
+    width: calc(100% - 30px)
+    padding-left: 15px
+    &Top
+      display: flex
+      margin-bottom: 5px
+    &Bottom
+      margin-bottom: 5px
+    &Buttons
+      display: flex
+      justify-content: flex-end
+      &Item
+        width: 100px
+        background-color: white
+        padding: 5px
+        text-align: center
+        margin-left: 10px
+        border-radius: 4px
+        &.-create
+          background-color: palegreen
+        .icon
+          vertical-align: text-bottom
+          margin-right: 5px
+  &__count
+    margin-right: 10px
+    width: 30px
+    &Label
+      font-size: 11px
+      width: 100%
+      text-align: center
+    &Button
+      background-color: white
+      width: 100%
+      height: 18px
+      border-radius: 9px
+      position: relative
+      &.-active
+        background-color: deepskyblue
+      &Circle
+        background-color: white
+        border: 1px solid #e2e2e2
+        width: 16px
+        height: 16px
+        border-radius: 7px
+        position: absolute
+        top: 1px
+        left: 1px
+        &.-active
+          left: unset
+          right: 1px
+    &Input
+      width: 100%
+      border: none
+      background-color: white
+      padding: 2px 3px
+      &[disabled=disabled]
+        color: gray
+        background-color: #eee
+  &__title
+    width: calc(100% - 40px)
+    &Label
+      font-size: 11px
+    &Input
+      border: none
+      width: 100%
+      height: 40px
+      padding: 7px
+      font-size: 16px
+  &__description
+    &Label
+      font-size: 11px
+    &Input
+      width: 100%
+      border: none
+      padding: 5px
+      font-size: 14px
+      height: 40px
+</style>
--- src/components/RoutinesColumn.vue
+++ src/components/RoutinesColumn.vue
@@ -16,23 +16,13 @@
       .routine__headerMenuItem.-hover(@click="mode = 'delete'")
         icon.icon(name="trash-alt")
         span Delete
-  .routine__create
-    .routine__createCheckbox
-    .routine__createForm
-      .routine__createFormTop
-        .routine__createCount
-          .routine__createCountLabel count
-          .routine__createCountButton.-hover(:class="{'-active': create.countActive}"
-            @click="create.countActive = !create.countActive")
-              .routine__createCountButtonCircle(:class="{'-active': create.countActive}")
-          input.routine__createCountInput(v-model="create.count" :disabled="!create.countActive" type="number")
-        .routine__createTitle
-          .routine__createTitleLabel title
-          input.routine__createTitleInput(v-model="create.title")
+  routine-create-form(v-if="mode === 'create'" @cancel="clearMode")
 </template>
 
 <script>
+import RoutineCreateForm from '~/components/RoutineCreateForm'
 export default {
+  components: { RoutineCreateForm },
   props: {
     type: { type: String, required: true }, // 'daily' | 'weekly' | 'monthly'
     dateLabel: { type: String, required: true }
@@ -40,12 +30,7 @@ export default {
   data () {
     return {
       showingMenu: false,
-      mode: null, // 'create' | 'edit' | 'delete'
-      create: {
-        countActive: false,
-        count: 1,
-        title: ''
-      }
+      mode: 'create' // 'create' | 'edit' | 'delete'
     }
   },
   computed: {
@@ -53,11 +38,9 @@ export default {
       return this.type.charAt(0).toUpperCase() + this.type.slice(1) + ' Routines'
     }
   },
-  watch: {
-    'create.countActive' () {
-      if (!this.create.countActive) {
-        this.create.count = 1
-      }
+  methods: {
+    clearMode () {
+      this.mode = null
     }
   }
 }
@@ -100,62 +83,4 @@ export default {
           width: 15px
           height: 15px
           margin-right: 5px
-  &__create
-    display: flex
-    padding: 5px 0
-    &Checkbox
-      border: 2px solid black
-      border-radius: 5px
-      width: 30px
-      height: 30px
-      background-color: white
-      cursor: pointer
-    &Form
-      width: calc(100% - 30px)
-      padding-left: 15px
-      &Top
-        display: flex
-    &Count
-      margin-right: 10px
-      &Label
-        font-size: 11px
-        width: 30px
-        text-align: center
-      &Button
-        background-color: white
-        width: 30px
-        height: 18px
-        border-radius: 9px
-        position: relative
-        &.-active
-          background-color: deepskyblue
-        &Circle
-          background-color: white
-          border: 1px solid #e2e2e2
-          width: 16px
-          height: 16px
-          border-radius: 7px
-          position: absolute
-          top: 1px
-          left: 1px
-          &.-active
-            left: unset
-            right: 1px
-      &Input
-        width: 30px
-        border: none
-        background-color: white
-        padding: 2px 3px
-        &[disabled=disabled]
-          color: gray
-          background-color: #eee
-    &Title
-      &Label
-        font-size: 11px
-      &Input
-        border: none
-        height: 40px
-        width: 100%
-        padding: 7px
-        font-size: 16px
 </style>
