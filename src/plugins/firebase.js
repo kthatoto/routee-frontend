2462bb0ff6d4454171fb98ccd8605fab3ce161e7
kthatoto <kthatoto@gmail.com>
2019-12-30 15:24:08 +0900

    Add firebase config

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -9,7 +9,9 @@ if (!firebase.apps.length) {
       databaseURL: process.env.FIREBASE_DATABASEURL,
       projectId: process.env.FIREBASE_PROJECTID,
       storageBucket: process.env.FIREBASE_STORAGEBUCKET,
-      messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID
+      messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID,
+      appId: process.env.FIREBASE_APPID,
+      measurementId: process.env.FIREBASE_MEASUREMENTID
     }
   )
 }
