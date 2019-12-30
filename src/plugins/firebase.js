a9b9d876789506308a7978655183da857458824d
kthatoto <kthatoto@gmail.com>
2019-12-30 19:06:27 +0900

    Debug

--- src/plugins/firebase.js
+++ src/plugins/firebase.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app'
 import 'firebase/auth'
 
 if (!firebase.apps.length) {
+  console.log(process.env.FIREBASE_APIKEY)
   firebase.initializeApp(
     {
       apiKey: process.env.FIREBASE_APIKEY,
