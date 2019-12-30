9903e5a2a43960830d8d84869cac5a8635ed1789
kthatoto <kthatoto@gmail.com>
2019-12-30 19:27:46 +0900

    Env in config

--- nuxt.config.js
+++ nuxt.config.js
@@ -5,6 +5,17 @@ dotenv.config()
 export default {
   mode: 'spa',
   srcDir: 'src',
+  env: {
+    SERVER_URL: process.env.SERVER_URL,
+    FIREBASE_APIKEY: process.env.FIREBASE_APIKEY,
+    FIREBASE_AUTHDOMAIN: process.env.FIREBASE_AUTHDOMAIN,
+    FIREBASE_DATABASEURL: process.env.FIREBASE_DATABASEURL,
+    FIREBASE_PROJECTID: process.env.FIREBASE_PROJECTID,
+    FIREBASE_STORAGEBUCKET: process.env.FIREBASE_STORAGEBUCKET,
+    FIREBASE_MESSAGINGSENDERID: process.env.FIREBASE_MESSAGINGSENDERID,
+    FIREBASE_APPID: process.env.FIREBASE_APPID,
+    FIREBASE_MEASUREMENTID: process.env.FIREBASE_MEASUREMENTID
+  },
   head: {
     title: process.env.npm_package_name || '',
     meta: [
