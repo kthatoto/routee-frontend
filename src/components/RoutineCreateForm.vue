e5f019f1d005f58271687eec14e2418180628def
kthatoto <kthatoto@gmail.com>
2020-01-17 14:40:25 +0900

    Add datepickers to create routine form

--- src/components/RoutineCreateForm.vue
+++ src/components/RoutineCreateForm.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
 el-card.create
-  el-form.create__form(label-width="85px" :model="form" ref="form" :rules="rules")
+  el-form.create__form(label-width="95px" :model="form" ref="form" :rules="rules")
     el-form-item(label="count")
       el-switch.create__countSwitch(v-model="form.countActive")
       el-input-number.create__countInput(v-model="form.count" :disabled="!form.countActive" size="mini" :min="1")
@@ -8,6 +8,12 @@ el-card.create
       el-input(v-model="form.name")
     el-form-item(label="description")
       el-input(type="textarea" v-model="form.description")
+    el-form-item(:label="`start ${dateTypeSlug}`" prop="startDate")
+      el-date-picker.create__datepicker(v-model="form.startDate" :clearable="false" :editable="false"
+        :type="dateTypeSlug" :format="datepickerFormat")
+    el-form-item(:label="`end ${dateTypeSlug}`" prop="endDate" :error="errors.endDate")
+      el-date-picker.create__datepicker(v-model="form.endDate" :editable="false"
+        :type="dateTypeSlug" :format="datepickerFormat")
     .create__buttons
       el-button(@click="cancel")
         icon.icon.-l(name="ban")
@@ -29,19 +35,33 @@ export default {
         countActive: false,
         count: 1,
         name: '',
-        description: ''
+        description: '',
+        startDate: null,
+        endDate: null
       },
       rules: {
-        name: [
-          { required: true, message: 'Name is required' }
-        ]
+        name: { required: true },
+        startDate: { type: 'date', required: true, message: 'start date is required' }
       },
       errors: {
-        name: null
+        name: null,
+        endDate: null
       }
     }
   },
   computed: {
+    formattedStartDate () {
+      return this.form.startDate ? this.$dayjs(this.form.startDate).format('YYYY-MM-DD') : null
+    },
+    formattedEndDate () {
+      return this.form.endDate ? this.$dayjs(this.form.endDate).format('YYYY-MM-DD') : null
+    },
+    dateTypeSlug () {
+      return { daily: 'date', weekly: 'week', monthly: 'month' }[this.type]
+    },
+    datepickerFormat () {
+      return { daily: 'yyyy-MM-dd', weekly: 'yyyy-MM-dd', monthly: 'yyyy-MM' }[this.type]
+    },
     ...mapGetters({
       date: 'getDate'
     })
@@ -51,26 +71,54 @@ export default {
       if (!this.form.countActive) {
         this.form.count = 1
       }
+    },
+    'form.startDate' () {
+      this.adjustDatesToType('start')
+    },
+    'form.endDate' () {
+      this.adjustDatesToType('end')
     }
+  },
+  created () {
+    this.form.startDate = new Date()
   },
   methods: {
     cancel () {
       this.$emit('cancel')
     },
+    adjustDatesToType (dateName) {
+      if (this.type === 'weekly' || this.type === 'monthly') {
+        const beforeDate = this.form[`${dateName}Date`]
+        const adjustedDate = this.$dayjs(beforeDate)[`${dateName}Of`](this.dateTypeSlug).toDate()
+        if (!this.$dayjs(beforeDate).isSame(this.$dayjs(adjustedDate))) {
+          this.form[`${dateName}Date`] = adjustedDate
+        }
+      }
+    },
     clearErrors () {
       this.errors.name = null
+      this.errors.endDate = null
+    },
+    customValidate () {
+      if (this.form.endDate && this.form.startDate > this.form.endDate) {
+        this.errors.endDate = 'end date must be before start date'
+      }
+      return Object.values(this.errors).every(v => v === null)
     },
     async submit () {
-      const valid = await this.$refs.form.validate()
-      if (!valid) { return }
       this.clearErrors()
+      const valid = await this.$refs.form.validate()
+      const customValid = await this.customValidate()
+      if (!(valid && customValid)) { return }
       this.$apiClient('post',
         '/routines',
         {
           interval_type: this.type,
           count: this.form.count,
           name: this.form.name,
-          description: this.form.description
+          description: this.form.description,
+          start_date: this.formattedStartDate,
+          end_date: this.formattedEndDate
         }
       ).then(() => {
         this.$emit('done')
@@ -96,6 +144,8 @@ export default {
   &__buttons
     display: flex
     justify-content: flex-end
+  &__datepicker
+    width: 100%
   >>>
    .el-card__body
      padding: 10px
